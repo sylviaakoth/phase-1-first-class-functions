a3c7e64a3549a9010473dfda435f3b1334493dc6
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-22 14:11:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+     return callback()
+
+}
+function returnsANamedFunction() {
+    return returnsANamedFunction
+}
+    
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    }
+
+}
